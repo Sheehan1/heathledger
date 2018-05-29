f6b38e5fc6eebc6618c511d0a5b218ddba51ddd5
Sheehan1 <39743046+Sheehan1@users.noreply.github.com>
2018-05-29 21:21:49 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -6,15 +6,29 @@ bot.on('ready', function () {
     console.log("Je suis connecté !");
 });
 
-bot.login(process.env.TOKEN);
+bot.login("MjQzODQ5NDE5MzQxMDM3NTY5.De8TvQ.msKJeBk6yyDhhPQnbLt4D9UiE5k");
 bot.on('message', message => {
     if (message.content === prefix + "help") {
-        message.channel.sendMessage("Liste des commandes: \n -*help");
+        message.channel.sendMessage("Liste des commandes: \n -*help\n-*Créateur\n-*embed");
     }
 
     if (message.content === "Salut") {
         message.reply("Bien le Bonjours. :)");
         console.log("Commande Salut effectué ");
-
     }
+
+    if (message.content === prefix + "Créateur") {
+        message.channel.sendMessage("J'ai J'ai été créé par Majesté. ");
+    }
+    if (message.content === prefix + "embed") {
+        var embed = new Discord.RichEmbed()
+            .setTitle("EMBED")
+            .setDescription("Ceci est un embed")
+            .addField(".help", "Page d'aide", true)
+            .addField("Embed01", "Embed 01 ! :)", true)
+            .setColor("0x#000029")
+            .setFooter("Bon Moment parmis nous! ")
+        MessageChannel.sendEmbed(embed);
+}
+
 });
