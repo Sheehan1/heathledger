a8df387a739db4857101df3e8b1d32e75414c417
Sheehan1 <39743046+Sheehan1@users.noreply.github.com>
2018-05-29 22:07:04 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -6,7 +6,7 @@ bot.on('ready', function () {
     console.log("Je suis connecté !");
 });
 
-bot.login(precess.env.TOKEN);
+bot.login(process.env.TOKEN);
 bot.on('message', message => {
     if (message.content === prefix + "help") {
         message.channel.sendMessage("Liste des commandes: \n *help \n *Créateur \n *embed");
