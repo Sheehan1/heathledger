72ed81c9ac30d510e727501c28d0db171861ced4
Sheehan1 <39743046+Sheehan1@users.noreply.github.com>
2018-05-30 22:33:53 +0200

    Update app.js

--- .gitignore/app.js
+++ .gitignore/app.js
@@ -1,20 +1,46 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const settings = ('MjQzODQ5NDE5MzQxMDM3NTY5.De8TvQ.msKJeBk6yyDhhPQnbLt4D9UiE5k');
+const settings = require('./settings.json').token;
 const prefix = require ('./settings.json').prefix;
-//const chalk = require('chalk');
-//const moment = require('moment');
+const chalk = require('chalk');
+const moment = require('moment');
 require('./util/eventLoader')(client);
 
 
-client.login(settings);
-
 client.commands = new Discord.Collection();
-//client.aliases = new Discord.Collection();
+client.aliases = new Discord.Collection();
 client.on('message', message => {
   let args = message.content.split(' ').slice(1);
   var result = args.join(' ');
 
+  if (!message.content.startsWith(prefix)) return;
+  if (message.author.bot) return;
+
+  if (message.content.startsWith(prefix + 'join')) {
+		let voiceChan = message.member.voiceChannel;
+		if (!voiceChan || voiceChan.type !== 'voice') {
+			message.channel.sendMessage('No').catch(error => message.channel.sendMessage(error));
+		} else if (message.guild.voiceConnection) {
+			message.channel.sendMessage('I\'m already in a voice channel');
+		} else {
+			message.channel.sendMessage('Joining...').then(() => {
+				voiceChan.join().then(() => {
+					message.channel.sendMessage('Joined successfully.').catch(error => message.channel.sendMessage(error));
+				}).catch(error => message.channel.sendMessage(error));
+			}).catch(error => message.channel.sendMessage(error));
+		}
+	} else
+
+	if (message.content.startsWith(prefix + 'leave')) {
+		let voiceChan = message.member.voiceChannel;
+		if (!voiceChan) {
+			message.channel.sendMessage('I am not in a voice channel');
+		} else {
+			message.channel.sendMessage('Leaving...').then(() => {
+				voiceChan.leave();
+			}).catch(error => message.channel.sendMessage(error));
+		}
+	}
 });
 
-
+client.login(settings);
