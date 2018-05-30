088c63c1a9d4456c1c51f1934dd2039120e82923
Sheehan1 <39743046+Sheehan1@users.noreply.github.com>
2018-05-30 20:22:56 +0200

    Update and rename app.js to index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,35 @@
+const Discord = require('discord.js')
+const bot = new Discord.Client()
+var prefix = ("*")
+bot.on('ready', function () {
+    bot.user.setGame("Command: *help");
+    console.log("Je suis connecté !");
+});
+
+bot.login(process.env.TOKEN);
+bot.on('message', message => {
+    if (message.content === prefix + "help") {
+        message.channel.sendMessage("Liste des commandes: \n *help \n *Créateur \n *embed");
+    }
+
+    if (message.content === "Salut") {
+        message.reply("Bien le Bonjours. :)");
+        console.log("Commande Salut effectué ");
+    }
+
+    if (message.content === prefix + "Créateur") {
+        message.channel.sendMessage("J'ai été créé par Majesté. ");
+    }
+    if (message.content === prefix + "embed") {
+        var embed = new Discord.RichEmbed()
+            .setTitle("EMBED")
+  .setAuthor("Sa majesté ", "https://man-man.nl/app/uploads/2017/01/Peaky-Blinders-Thomas-Shelby-MAN-MAN.jpg")
+  .setColor(0x00AE86)
+  .setDescription("Voici un récap des commandes.")
+  .setFooter("Crée par Sheehan")
+  .setImage("https://man-man.nl/app/uploads/2017/01/Peaky-Blinders-Thomas-Shelby-MAN-MAN.jpg")
+  .addField("*help \n *Créateur \n *Embed.")
+  message.channel.send({embed});
+     }
+
+});
