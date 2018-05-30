523767a7c12939c616b88baf09c009e68efffcc9
Sheehan1 <39743046+Sheehan1@users.noreply.github.com>
2018-05-30 02:32:26 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,7 +1,5 @@
 const Discord = require('discord.js')
 const bot = new Discord.Client()
-const Cleverbot = require("cleverbot-node");
-const clbot = new Cleverbot;
 var prefix = ("*")
 bot.on('ready', function () {
     bot.user.setGame("Command: *help");
@@ -60,14 +58,7 @@ bot.on('message', message => {
 			return message.reply("Tu n'as pas la permission.")
         }
     }
-   if (message.channel.type === "dm") {
-    clbot.write(message.content, (response) => {
-      message.channel.startTyping();
-      setTimeout(() => {
-        message.channel.send(response.output).catch(console.error);
-        message.channel.stopTyping();
-      }, Math.random() * (1 - 3) + 1 * 1000);
-    });
-  }
+
+  
 });
 
