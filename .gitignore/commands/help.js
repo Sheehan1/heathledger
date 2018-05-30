749b2a74bd2e3fb5fb227d3ca938346109c366b1
Sheehan1 <39743046+Sheehan1@users.noreply.github.com>
2018-05-30 21:38:57 +0200

    Delete help.js

--- .gitignore/commands/help.js
+++
@@ -1,8 +0,0 @@
-const settings = require('../settings.json');
-
-
-exports.run = (client, message, args) => {
-
-return message.reply(`\n${settings.prefix}Help - Menu Help \n${settings.prefix}Sondage <Question>\n${settings.prefix}-Clear <nombre de message> \n**[C'EST TOUT POUR LE MOMENT]**`)
-
-}
