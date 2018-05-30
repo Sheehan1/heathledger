190fcabec8f6f8a0609efcdab37b114443867c1a
Sheehan1 <39743046+Sheehan1@users.noreply.github.com>
2018-05-30 21:59:56 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,35 +1,46 @@
-const Discord = require('discord.js')
-const bot = new Discord.Client()
-var prefix = ("*")
-bot.on('ready', function () {
-    bot.user.setGame("Command: *help");
-    console.log("Je suis connecté !");
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const settings = require('./settings.json').token;
+const prefix = require ('./settings.json').prefix;
+const chalk = require('chalk');
+const moment = require('moment');
+require('./util/eventLoader')(client);
+
+
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+client.on('message', message => {
+  let args = message.content.split(' ').slice(1);
+  var result = args.join(' ');
+
+  if (!message.content.startsWith(prefix)) return;
+  if (message.author.bot) return;
+
+  if (message.content.startsWith(prefix + 'join')) {
+		let voiceChan = message.member.voiceChannel;
+		if (!voiceChan || voiceChan.type !== 'voice') {
+			message.channel.sendMessage('No').catch(error => message.channel.sendMessage(error));
+		} else if (message.guild.voiceConnection) {
+			message.channel.sendMessage('I\'m already in a voice channel');
+		} else {
+			message.channel.sendMessage('Joining...').then(() => {
+				voiceChan.join().then(() => {
+					message.channel.sendMessage('Joined successfully.').catch(error => message.channel.sendMessage(error));
+				}).catch(error => message.channel.sendMessage(error));
+			}).catch(error => message.channel.sendMessage(error));
+		}
+	} else
+
+	if (message.content.startsWith(prefix + 'leave')) {
+		let voiceChan = message.member.voiceChannel;
+		if (!voiceChan) {
+			message.channel.sendMessage('I am not in a voice channel');
+		} else {
+			message.channel.sendMessage('Leaving...').then(() => {
+				voiceChan.leave();
+			}).catch(error => message.channel.sendMessage(error));
+		}
+	}
 });
 
-bot.login(process.env.TOKEN);
-bot.on('message', message => {
-    if (message.content === prefix + "help") {
-        message.channel.sendMessage("Liste des commandes: \n *help \n *Créateur \n *embed");
-    }
-
-    if (message.content === "Salut") {
-        message.reply("Bien le Bonjours. :)");
-        console.log("Commande Salut effectué ");
-    }
-
-    if (message.content === prefix + "Créateur") {
-        message.channel.sendMessage("J'ai été créé par Majesté. ");
-    }
-    if (message.content === prefix + "embed") {
-        var embed = new Discord.RichEmbed()
-            .setTitle("EMBED")
-  .setAuthor("Sa majesté ", "https://man-man.nl/app/uploads/2017/01/Peaky-Blinders-Thomas-Shelby-MAN-MAN.jpg")
-  .setColor(0x00AE86)
-  .setDescription("Voici un récap des commandes.")
-  .setFooter("Crée par Sheehan")
-  .setImage("https://man-man.nl/app/uploads/2017/01/Peaky-Blinders-Thomas-Shelby-MAN-MAN.jpg")
-  .addField("*help \n *Créateur \n *Embed.")
-  message.channel.send({embed});
-     }
-
-});
+client.login(settings);
