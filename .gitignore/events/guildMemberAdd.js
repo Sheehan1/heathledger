e863d88ed353a7a25700efe7bfa84906d8f3bd98
Sheehan1 <39743046+Sheehan1@users.noreply.github.com>
2018-05-30 21:43:54 +0200

    Delete guildMemberAdd.js

--- .gitignore/events/guildMemberAdd.js
+++
@@ -1,7 +0,0 @@
-module.exports = member => {
-  const guild = member.guild;
-  const channel = guild.channels.find('name','général')
-  if (!channel) return;
-  channel.send(`Bienvenue dans la secte, ${member}`);
- 
-};
