03ded74ade5834ba82e923f80c06996f0f4b4417
Sheehan1 <39743046+Sheehan1@users.noreply.github.com>
2018-06-03 18:12:39 +0200

    Update Sondage.js

--- .gitignore/commands/Sondage.js
+++ .gitignore/commands/Sondage.js
@@ -1,3 +1,4 @@
 const Discord = require('discord.js');
 exports.run = (client, message, args) => {
- message.reply('**Mauvais formatt:** `*sondage<Question> , s minuscule `')
+ return.message.reply('**Mauvais formatt:** `*sondage<Question> , s minuscule `')
+}
