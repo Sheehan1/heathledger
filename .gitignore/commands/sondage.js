ba2e56f941972201da217c69b11396f12ac9e7b1
Sheehan1 <39743046+Sheehan1@users.noreply.github.com>
2018-06-03 18:25:53 +0200

    Update sondage.js

--- .gitignore/commands/sondage.js
+++ .gitignore/commands/sondage.js
@@ -14,8 +14,8 @@ exports.run = (client, message, args) => {
     .addField(ThingToEcho,"Répondre avec :thumbsup: ,:shrug:, :thumbsdown:")    
     .setFooter(`Sondage par: ${message.author.username}`, `${message.author.avatarURL}`)
     .setTimestamp()
-    message.channel.send({embed})
-   
+    //message.channel.send({embed})
+   client.channels.get(451140180108115979).send({embed})
    .then(function( message){
          message.react('👍')
          message.react('👎')
