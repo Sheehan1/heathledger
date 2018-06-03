d8f76101cf23cdb3aa7f2b6b2116acc92ed691f7
Sheehan1 <39743046+Sheehan1@users.noreply.github.com>
2018-06-03 18:08:20 +0200

    Update help.js

--- .gitignore/commands/help.js
+++ .gitignore/commands/help.js
@@ -3,6 +3,6 @@ const settings = require('../settings.json');
 
 exports.run = (client, message, args) => {
 
-return message.reply(`\n${settings.prefix}Help - Menu Help \n${settings.prefix}Sondage <Question>\n${settings.prefix}-Clear <nombre de message> \n**[C'EST TOUT POUR LE MOMENT]**`)
+return message.reply(`\n${settings.prefix}Help - Menu Help \n${settings.prefix}sondage <Question>\n${settings.prefix}clear <nombre de message> \n**[C'EST TOUT POUR LE MOMENT]**`)
 
 }
