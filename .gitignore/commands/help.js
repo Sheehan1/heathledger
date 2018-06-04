9e25ddf34867728260e301f1a69a72a19cc202f6
Sheehan1 <39743046+Sheehan1@users.noreply.github.com>
2018-06-04 19:08:42 +0200

    Update help.js

--- .gitignore/commands/help.js
+++ .gitignore/commands/help.js
@@ -1,8 +1,18 @@
 const settings = require('../settings.json');
-
+const Discord = require('discord.js');
 
 exports.run = (client, message, args) => {
 
-return message.reply(`\n${settings.prefix}Help - Menu Help \n${settings.prefix}sondage <Question>\n${settings.prefix}clear <nombre de message> \n**[C'EST TOUT POUR LE MOMENT]**`)
-
+    const embed = new Discord.RichEmbed()
+    .setTitle("Menu help ")
+    .setColor("#5599ff")
+    //.setDescription(`menu`)
+    .setThumbnail("https://la7emeseance.files.wordpress.com/2013/04/bc4.jpg")
+    .addField("Les différentes commande disponnible pour le peuple  ","*sondage <question>.. suite à venir  ")  
+    .addField("Commandes modération","*clear <Nombre de messages>..suite à venir")
+    //.setFooter(`Sondage par: ${message.author.username}`, `${message.author.avatarURL}`)
+    .setTimestamp()
+   //
+//return message.reply(`\n${settings.prefix}Help - Menu Help \n${settings.prefix}Sondage <Question>\n${settings.prefix}-Clear <nombre de message> \n**[C'EST TOUT POUR LE MOMENT]**`)
+message.channel.send({embed});
 }
